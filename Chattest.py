f71029ac740fd7c45e6bf82805f12631ba5bb68f
Ichiya Takashiro <sabongazin@gmail.com>
2024-03-06 02:34:39 +0900

    どうにか動作までこぎつけましたLoLaないと動作しないのね

---
+++ Chattest.py
@@ -0,0 +1,26 @@
+import torch
+from transformers import AutoTokenizer, AutoModelForCausalLM
+
+tokenizer = AutoTokenizer.from_pretrained("sanbongazin/willgpt-neox-small_v2", use_fast=False)
+model = AutoModelForCausalLM.from_pretrained("sanbongazin/willgpt-neox-small_v2",device_map="cuda:0",)
+
+if torch.cuda.is_available():
+    model = model.to("cuda")
+
+text = "配信をかわいいく縁取りするには"
+token_ids = tokenizer.encode(text, add_special_tokens=False, return_tensors="pt")
+
+with torch.no_grad():
+    output_ids = model.generate(
+        token_ids.to(model.device),
+        max_new_tokens=100,
+        min_new_tokens=100,
+        # do_sample=True,
+        temperature=0.8,
+        pad_token_id=tokenizer.pad_token_id,
+        bos_token_id=tokenizer.bos_token_id,
+        eos_token_id=tokenizer.eos_token_id
+    )
+
+output = tokenizer.decode(output_ids.tolist()[0])
+print(output)
---
+++ WillGPTV2.py
@@ -0,0 +1,117 @@
+# モデルの準備
+model_name = "rinna/japanese-gpt-neox-small"
+peft_name = "willgpt-neox-small"
+output_dir = "willgpt-neox-small-results"
+
+
+from transformers import Trainer, TrainingArguments, DataCollatorForLanguageModeling, AutoTokenizer, AutoModelForCausalLM
+import torch,transformers
+
+# トークナイザーの準備
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+
+from datasets import load_dataset
+dataset = load_dataset("kunishou/databricks-dolly-15k-ja")
+
+# dataset["train"] = dataset["train"].rename_column("instruction", "input_ids")
+# dataset["train"] = dataset["train"].rename_columen("output", "completion")
+
+# dataset["train"]["input_ids"] = dataset["train"]["input_ids"]
+# dataset["train"]["completion"] = dataset["train"]["completion"]
+
+data=dataset["train"]
+formatted_data=[]
+
+CUTOFF_LEN = 256  # コンテキスト長
+# トークナイズ関数
+def tokenize(prompt, tokenizer):
+    result = tokenizer(
+        prompt,
+        truncation=True,
+        max_length=CUTOFF_LEN,
+        padding=False,
+    )
+    return {
+        "input_ids": result["input_ids"],
+        "attention_mask": result["attention_mask"],
+    }
+
+# プロンプトテンプレートの準備
+# def generate_prompt(data_point):
+#     result = f"""### 指示:
+#     {data_point["input"]}
+
+# ### 回答:
+#     {data_point["completion"]}
+#     """
+#     # 改行→<NL>
+#     result = result.replace('\n', '<NL>')
+#     return result
+
+for i,j in zip(data["instruction"],data["output"]):
+    formatted_data.append(tokenize(i,tokenizer))
+
+
+from transformers import AutoModelForCausalLM, Trainer, TrainingArguments
+
+# モデルの準備
+model = AutoModelForCausalLM.from_pretrained(
+    model_name,
+    device_map="cuda:0",
+    torch_dtype=torch.float16,
+)
+
+from peft import LoraConfig, get_peft_model, prepare_model_for_int8_training, TaskType
+
+# LoRAのパラメータ
+lora_config = LoraConfig(
+    inference_mode=False, 
+    r=8, lora_alpha=32, 
+    lora_dropout=0.1
+    )
+
+# モデルの前処理
+model = prepare_model_for_int8_training(model)
+
+# LoRAモデルの準備
+model = get_peft_model(model, lora_config)
+
+
+# 学習可能パラメータの確認
+model.print_trainable_parameters()
+
+
+
+training_args = TrainingArguments(
+    output_dir=output_dir,
+    per_device_train_batch_size=6,
+    num_train_epochs=3,
+    logging_dir='./logs',
+    remove_unused_columns=False
+)
+
+trainer = transformers.Trainer(
+    model=model,
+    train_dataset=formatted_data,
+    args = transformers.TrainingArguments(
+    output_dir=output_dir,
+    num_train_epochs=6,
+    #勾配と学習率を設定
+    gradient_accumulation_steps = 2,
+    learning_rate = 5e-5,
+    per_device_train_batch_size=4,
+    logging_dir="./logs",
+    logging_steps=500,
+    save_steps=5000,
+    overwrite_output_dir=True,
+    remove_unused_columns=False,
+    
+),
+    data_collator=transformers.DataCollatorForLanguageModeling(tokenizer, mlm=False),
+)
+
+trainer.train()
+# LoRAモデルの保存
+trainer.model.save_pretrained(peft_name,push_to_hub=True)
+tokenizer.save_pretrained(peft_name,push_to_hub=True)
+
