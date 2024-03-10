6d9e4236e87173446ffdaa7c81f560a9a8fa68cc
Ichiya Akihoshi <sanbongazin@gmail.com>
2024-03-10 12:01:00 +0900

    テストをいろいろやってみてる

--- ChatTest_calm.py
+++ ChatTest_calm.py
@@ -7,6 +7,7 @@ assert transformers.__version__ >= "4.34.1"
 token="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"
 # model_name="sanbongazin/willgpt-neox-small_v2"
 model_name="sanbongazin/willgpt-open-calm-1b"
+# model_name="sanbongazin/willgpt-Gemma"
 
 try:
     model = AutoModelForCausalLM.from_pretrained(model_name, device_map="cpu", low_cpu_mem_usage=True)
@@ -16,7 +17,7 @@ except:
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 streamer = TextStreamer(tokenizer, skip_prompt=True, skip_special_tokens=True,token=token)
 
-prompt = "OBSのインストールは"
+prompt = "配信画面を可愛くするには"
 
 token_ids = tokenizer.encode(prompt, return_tensors="pt")
 output_ids = model.generate(
